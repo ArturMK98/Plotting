0120e98ed99fed93cebdf4b8bfa47f509c21ef5c
artur <karolewski.artur@gmail.com>
2025-02-10 21:10:45 +0000

    Graphed highs, added new file for graphing highs and lows, added exception handling and added a file to map rainfall

--- CSV Data/mullingar_highs_lows.py
+++ CSV Data/mullingar_highs_lows.py
@@ -4,7 +4,8 @@ import csv
 
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/mullingar_weather_december_2024.csv')
+path = Path('weather_data/mullingar_weather_2024_full_missing_data.csv')
+path = Path('weather_data/mullingar_weather_2024_full.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
@@ -13,25 +14,29 @@ for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 # Extract temperature data
-dates, highs = [], []
+dates, highs, lows = [], [], []
 for row in reader:
     date = datetime.strptime(str(row[0]), '%d-%b-%Y')
     try:
         high = float(row[2])
+        low = float(row[3])
     except ValueError:
         print(f"Missing data for {date}")
     else:
         dates.append(date)
         highs.append(high)
+        lows.append(low)
 
 
 # Plot high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red')
+ax.plot(dates, highs, color='red', label="Highs", alpha=0.5)
+ax.plot(dates, lows, color='blue', label="Lows", alpha=0.5)
+ax.fill_between(dates, highs, lows, facecolor='blue', alpha=0.1)
 
 # Format plot
-ax.set_title("Daily High Temperatures, December 2024", fontsize=24)
+ax.set_title("Daily High and Low Temperatures, Mullingar 2024", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
 ax.set_ylabel("Temperature (C)", fontsize=16)
--- CSV Data/mullingar_rainfall.py
+++ CSV Data/mullingar_rainfall.py
@@ -4,7 +4,7 @@ import csv
 
 import matplotlib.pyplot as plt
 
-path = Path('weather_data/mullingar_weather_december_2024.csv')
+path = Path('weather_data/mullingar_weather_2024_full.csv')
 lines = path.read_text(encoding='utf-8').splitlines()
 reader = csv.reader(lines)
 header_row = next(reader)
@@ -13,28 +13,28 @@ for index, column_header in enumerate(header_row):
     print(index, column_header)
 
 # Extract temperature data
-dates, highs = [], []
+dates, rain = [], []
 for row in reader:
     date = datetime.strptime(str(row[0]), '%d-%b-%Y')
     try:
-        high = float(row[2])
+        rainfall = float(row[1])
     except ValueError:
         print(f"Missing data for {date}")
     else:
         dates.append(date)
-        highs.append(high)
+        rain.append(rainfall)
 
 
 # Plot high temperatures
 plt.style.use('seaborn-v0_8')
 fig, ax = plt.subplots()
-ax.plot(dates, highs, color='red')
+ax.plot(dates, rain, color='purple', label="Rainfall", alpha=0.5)
 
 # Format plot
-ax.set_title("Daily High Temperatures, December 2024", fontsize=24)
+ax.set_title("Daily Rainfall, Mullingar 2024", fontsize=24)
 ax.set_xlabel('', fontsize=16)
 fig.autofmt_xdate()
-ax.set_ylabel("Temperature (C)", fontsize=16)
+ax.set_ylabel("Rain (mm)", fontsize=16)
 ax.tick_params(labelsize=16)
 
 ax.legend()
